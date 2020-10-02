46306a8b2dcce1441a29a760265237f29d821983
Sujoy Datta <sujoydatta.2009@gmail.com>
2020-10-02 22:12:45 +0530

    Changes

--- Multiprocessing/4.c
+++ Multiprocessing/4.c
@@ -1,95 +1,134 @@
 #include<stdio.h>
 #include<unistd.h>
-#include<sys/types.h>
-#include<sys/wait.h>
-#include<stdlib.h>
-#include<ctype.h>
-#include<math.h>
-#include<stdlib.h>
 #include<string.h>
-#include<wait.h>
+#include<stdlib.h>
+#include<sys/wait.h>
+#define maxcmd 10
 
+char *argp[100];
+int arglen;
+char buf[100][100];
+int buflen;
 
-void history(char his[], char cmd[]) 
+char PrevCmd[maxcmd][500];
+int cmdno;
+
+void GetArgs(char c[])
 {
-        strcat(his, "\n");
-        strcat(his, cmd);
+	int i=0;
+	
+	arglen = 0;
+	
+	buflen = 0;
+
+	int eol = 0;
+	
+	while(eol == 0)
+	{
+		if(c[i] == '\0')
+		{
+			eol = 1;
+			
+			buf[arglen][buflen] = '\0';
+				
+			argp[arglen] = buf[arglen];
+			arglen++;
+			
+			buflen = 0;
+		}
+		
+		else 
+		{
+			if(c[i] == ' ')
+			{
+				buf[arglen][buflen] = '\0';
+				
+				argp[arglen] = buf[arglen];
+				arglen++;
+
+				buflen = 0;
+			}
+			
+			else 
+			{
+				buf[arglen][buflen] = c[i];
+				buflen++;
+			}
+		}
+		
+		i++;
+	}
+	
+	argp[arglen] = NULL;
 }
-int main() {
-        char cwd[128];
-        char his[1000] = "";
-        char rec[1];
-        char cmd[100] = {
-                0
-        };
-        int top = 0;
-        int fd[2];             //inter process communication
-        printf("\nSujoy's command shell\n\n");
-        while (1) 
-        {
-                if (getcwd(cwd, sizeof(cwd)) == NULL) 
-                {
-                        perror("getcwd() error");
-                        return 1;
-                }
 
-                pipe(fd);
-                printf("\n");
-                printf("\033[1;32m");
-                printf("Different:");
-                printf("\033[0m");
-                printf("\033[1;34m");
-                printf("~%s", cwd);
-                printf("\033[0m");
-                printf("@ ");
-                fflush(stdout);
-                scanf("%[^\n]%*c", cmd);
+void DisplayHistory(int h)
+{
+	printf("\n");
+	
+	for(int i=0;i<h && i<maxcmd && cmdno > i;i++)
+	{
+		int j = (cmdno-1-i)%maxcmd;
+		printf("%s\n", PrevCmd[j]);
+	}
+	
+	printf("\n");
+}
 
-                if (strcmp(cmd, "history"))
-                        printf("\n");
-                history(his, cmd);
-                if (strcmp(cmd, "exit") == 0) {
-                        break;
-                }
-                char arg[10][100] = {
-                        0
-                };
-                int argc = 0, count = 0;
-                for (int i = 0; i < strlen(cmd); i++) {
-                        if (cmd[i] == ' ') {
-                                argc++;
-                                count = 0;
-                        } else {
-                                arg[argc][count++] = cmd[i];
-                        }
-                }
-                char * argv[10] = {
-                        0
-                };
-                int k = 0;
-                for (k = 0; k <= argc; k++) {
-                        argv[k] = arg[k];
-                }
-                argv[k] = NULL;
-                pid_t pid = fork();
-                if (pid < 0) {
-                        exit(1);
-                } else if (pid == 0) {
-                        if (!strcmp(cmd, "history")) {
-                                printf("%s\n", his);
-                        } else {
-                                dup2(fd[1], 1);
-                                if (execvp( * argv, argv) == -1)
-                                        printf("Error executing \n");
-                        }
-                        exit(0);
-                } else {
-                        wait(NULL);
-                        close(fd[1]);
-                        while (read(fd[0], rec, 1) > 0) {
-                                printf("%c", rec[0]);
-                        }
-                        close(fd[0]);
-                }
-        }
+int main()
+{
+	char cmd[500];
+	
+	cmdno = 0;
+
+	while(1)
+	{
+		printf("@ ");
+		scanf(" %[^\n]", cmd);
+
+		if(strcmp(cmd, "exit") == 0)
+		{
+			return 0;
+		}
+		
+		else if(cmd[0] == '!')
+		{
+			int h;
+			if (!cmd[2])
+				h = (int)cmd[1] - 48;
+			else
+			{
+				int x = (int)cmd[1] - 48;
+				int y = (int)cmd[2] - 48;
+				h = 10*x + y;
+			}
+			
+			DisplayHistory(h);
+		}
+		
+		else 
+		{	
+			int pid = vfork();
+			
+			if(pid == 0)
+			{
+				strcpy(PrevCmd[cmdno%maxcmd], cmd);
+				cmdno++;
+			
+				GetArgs(cmd);
+				
+				if(execvp(argp[0], argp) == -1)
+				{
+					printf("%s: command not found.\n", argp[0]);
+				}
+				exit(0);
+			}
+			
+			else 
+			{
+				wait(NULL);
+				printf("\n");
+			}
+		}
+	}
 }
