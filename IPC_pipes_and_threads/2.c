c5cb265cc11989cbc0e54eb185afc6f9f62cd48d
Sujoy Datta <sujoydatta.2009@gmail.com>
2020-10-19 19:52:25 +0530

    changes

--- IPC_pipes_and_threads/2.c
+++ IPC_pipes_and_threads/2.c
@@ -8,9 +8,9 @@ int main(int argc, char const *argv[])
 {
 	int pipefd1[2],pipefd2[2];
 	int status1, status2;
-	char str1[20]="SujoyDatta";
-	char str2[20]="AbhayGupta";
-	char str4[20];
+	char str1[3]="abc";
+	char str2[3]="def";
+	char str4[6];
 	status1= pipe(pipefd1);
 	if(status1==-1)
 	{
@@ -29,7 +29,7 @@ int main(int argc, char const *argv[])
 		//child
 		close(pipefd1[RD_END]);
 		close(pipefd2[WR_END]);
-		write(pipefd1[WR_END],str2,sizeof(str2));
+		write(pipefd1[WR_END],str2,sizeof(str2)+1);
 		read(pipefd2[RD_END],str4,sizeof(str4));
 		printf("The concatenated string sent by the parent is %s\n",str4);
 	}
@@ -40,16 +40,16 @@ int main(int argc, char const *argv[])
 		close(pipefd2[RD_END]);
 		read(pipefd1[RD_END],str2,sizeof(str2));int k=0;
 		//concat
-		for(int i=0;i<10;i++)
+		for(int i=0;i<3;i++)
 		{
 			str4[i]=str1[i];
 		}
-		for(int j=10;j<20;j++)
+		for(int j=3;j<6;j++)
 		{
 			str4[j]=str2[k];
 			k++;
 		}
-		write(pipefd2[WR_END],str4,sizeof(str4));
+		write(pipefd2[WR_END],str4,sizeof(str4)+1);
 	}
 	return 0;
 }
